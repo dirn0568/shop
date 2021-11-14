b168afaa2efafc2046ae64d26ce6670679a256e4
dirn0568 <dirn0568@naver.com>
2021-11-14 17:38:58 +0900

    maian

--- pickme/settings.py
+++ pickme/settings.py
@@ -29,12 +29,12 @@ def read_secret(secret_name):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('SECRET_KEY')
+SECRET_KEY = 'django-insecure-8%%z#6d#$0beu+-xtpdugjm+q%-=%k964m5eblz=ro33l=o@_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL='accountapp.MyUser'
 # Application definition
