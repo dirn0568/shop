ac4f58588ca855e1231d1b01a365d15c54cf12ba
dirn0568 <dirn0568@naver.com>
2021-12-24 23:52:35 +0900

    12-24

---
+++ static/js/test_test3.js
@@ -0,0 +1,31 @@
+
+// function test() {
+            // console.log("실행중31341")
+            // var gender = document.getElementsById('etc_view');
+            // var chkValue = input[type=radio][id="cs_channel_etc"]:checked.val();
+            // if(chkValue){
+               // etc_view.css('display','none');
+            // }else{
+              //  etc_view.css('display','block');
+            // }
+
+            // var genderChoice;
+            // for (var i=0; i<gender.length; i++) {
+                // if(gender[i].checked) {
+                    // genderChoice = gender[i].value;
+                    // console.log("실행중")
+                // }
+            // }
+
+            // var chk_radio = document.getElementsByName('cs_channel');
+            // var sel_type = null;
+
+            // for(var i=0;i<chk_radio.length;i++){
+                // if(chk_radio[i].checked == true){
+                    // etc_view.style('display','none');
+                // } else {
+                    // etc_view.style('display','block');
+                // }
+            // }
+        //  }
+        // setInterval(test, 1000);
