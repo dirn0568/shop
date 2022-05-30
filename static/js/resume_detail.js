38214871afa4aa25c626e47c7343b19bf844a6be
dirn0568 <dirn0568@naver.com>
2022-05-30 21:08:51 +0900

    an0530

--- static/js/resume_detail.js
+++ static/js/resume_detail.js
@@ -1,11 +1,11 @@
 var count = 0;
 console.log("02-07실행중")
 
-function resume_propose(propose, pk){
+setTimeout(function resume_propose(propose, pk){
     if (!confirm("확인(예) 또는 취소(아니오)를 선택해주세요.")) {
         alert("취소 되었습니다.");
     } else {
         alert("면접제의 메일을 성공적으로 보냈습니다.");
-        setTimeout(window.location.href='http://3.39.239.247/resume/detail_resume/' + pk + '?resume_propose=' + pk, 1000);
+        window.location.href='http://3.39.239.247/resume/detail_resume/' + pk + '?resume_propose=' + pk;
     }
-}
+}, 1000);
