6eefc6e03b19ebd699aeeca35f849e4ec06ec19a
dirn0568 <dirn0568@naver.com>
2022-05-31 00:35:01 +0900

    an05311234

--- static/js/propose_detail.js
+++ static/js/propose_detail.js
@@ -7,11 +7,10 @@ function propose_okay(propose, title, pk){
         console.log(temp);
         alert("취소 되었습니다.");
     } else {
-        alert("휴대폰 번호와 이메일을 성공적으로 보냈습니다.");
         temp = 'http://3.39.239.247/message/message_propose_detail/' + title + '/' + pk + '?detail=1';
         console.log(temp);
-        window.open('http://3.39.239.247/message/message_propose_detail/' + title + '/' + pk + '?detail=1', "네이버" ,"top=100px, left=100px, height=800px, width=1200px");
         window.location.href='http://3.39.239.247/message/message_propose_detail/' + title + '/' + pk + '?detail=1';
+        alert("휴대폰 번호와 이메일을 성공적으로 보냈습니다.");
     }
 }
 
@@ -19,10 +18,9 @@ function propose_okay2(propose, title, pk){
     if (!confirm("정말로 면접제의를 거절하시겠습니까.(거절 메세지가 제안자에게 전달됩니다)")) {
         alert("취소 되었습니다.");
     } else {
-        alert("거절하셨습니다");
         temp = 'http://3.39.239.247/message/message_propose_detail/' + title + '/' + pk + '?detail=2';
         console.log(temp);
-        window.open('http://3.39.239.247/message/message_propose_detail/' + title + '/' + pk + '?detail2=1', "네이버" ,"top=100px, left=100px, height=800px, width=1200px");
         window.location.href='http://3.39.239.247/message/message_propose_detail/' + title + '/' + pk + '?detail2=1';
+        alert("거절하셨습니다");
     }
 }
