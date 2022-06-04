450f013b8e1e6fbde60f4719dbeef065639f0cfd
dirn0568 <dirn0568@naver.com>
2022-06-05 00:50:14 +0900

    an06051250

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class SearchReportForm(forms.Form):
-    search_word = forms.CharField(label='Search Word',
+    search_word = forms.CharField(label='아이디 검색',
                                   widget=forms.TextInput(attrs={'autofocus': 'autofocus',
                                                                 'autocomplete': 'off',
                                                                 'size': '40',
